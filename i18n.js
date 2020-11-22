97e591106d9aa83e2208937afaca02c433c54dad
Vladimir Shabanov <volodyashab@gmail.com>
2020-11-22 04:51:29 +0300

    Test filters

---
+++ i18n.js
@@ -0,0 +1,13 @@
+const NextI18Next = require('next-i18next').default
+const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
+const path = require('path')
+
+module.exports = new NextI18Next({
+  otherLanguages: ['en', 'ja'],
+  defaultLanguage: 'en',
+  localeSubpaths: {
+    en: 'en',
+    ja: 'jp',
+  },
+  localePath: path.resolve('./public/static/locales')
+})
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import { withTranslation, Router, i18n } from '../i18n'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import map from 'lodash/map'
+import concat from 'lodash/concat'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Index({response, error, t}) {
+  error && console.log('error', error)
+  const router = useRouter()
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    setQuestions(concat(questions, response.questions))
+  }, [response]);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Locale-SSR</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div>Current lang context: {}</div>
+      <div>Current lang translation: {t('language')}</div>
+      <div>page : {router.query.page || 1}</div>
+      {questions && map(questions, (question, key) => (
+        <div key={key}>
+          <div>{question.title}</div>
         </div>
-      </main>
+      ))}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <button onClick={() => i18n.changeLanguage('en')}>to en</button>
+      <button onClick={() => i18n.changeLanguage('ja')}>to ja</button>
+      <button onClick={
+        () => Router.push({
+          pathname: '/',
+          query: {
+            order: Router.query.order,
+            page: parseInt(Router.query.page) + 1 || 2
+          }
+        })}
+      >+1</button>
+      <button onClick={() => Router.push({pathname: '/', query: {order: 'most_voted', page: Router.query.page}})}>most
+        voted
+      </button>
+      <button onClick={() => Router.push({pathname: '/', query: {order: 'most_visited', page: Router.query.page}})}>most
+        visited
+      </button>
     </div>
   )
 }
+
+Index.getInitialProps = async({req, res, query, ...ctx}) => {
+  const language = req ? req.i18n.language : i18n.language
+  const response = await axios.get(`https://api.igaming8.com/api/web/v1/questions?page=${query.page || 1}&per=5&order=${query.order || ''}&locale=${language}`)
+  return {
+    namespacesRequired: ['common'],
+    response: !response.error && response.data,
+    error: response.error && response.error,
+  }
+}
+
+export default withTranslation('common')(Index)
